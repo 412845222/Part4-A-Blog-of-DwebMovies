d52eaf8d2e66a887df4165edce10e6d453a80523
412845222 <412845222@qq.com>
2020-07-09 01:50:41 +0800

    master

--- dwebhtml/src/views/AddArticle.vue
+++ dwebhtml/src/views/AddArticle.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-article">
     <el-row :gutter="10">
-      <el-col :xs="24" :lg="12">
+      <el-col :xs="24" :lg="8">
         <div class="dweb">
           <el-form
             :label-position="'left'"
@@ -12,12 +12,12 @@
               <el-input v-model="article_info.title"></el-input>
             </el-form-item>
             <el-form-item label="描述">
-              <el-input v-model="article_info.describe"></el-input>
+              <el-input type="textarea" v-model="article_info.describe"></el-input>
             </el-form-item>
           </el-form>
         </div>
       </el-col>
-      <el-col :xs="24" :lg="12">
+      <el-col :xs="24" :lg="16">
         <div class="dweb"></div>
       </el-col>
       <el-col :xs="24" :lg="24">
